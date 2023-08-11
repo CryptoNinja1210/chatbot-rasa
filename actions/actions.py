66aca2c2bd092bd7a93794668118964de513f3d1
CrryptoNinja0617 <crypto.ninja1210@gmail.com>
2023-08-12 07:04:33 +0900

    endpoint

--- actions/actions.py
+++ actions/actions.py
@@ -12,16 +12,20 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-
-class ActionHelloWorld(Action):
-
+class ActionAddDestinaton(Action):
     def name(self) -> Text:
-        return "action_hello_world"
-
+        return "action_add_destination"
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
-
-        return ["Hello World!"]
+        entity_to = next(tracker.get_latest_entity_values(entity_type="location", entity_role="to"), None)
+        slot_main_destination = tracker.get_slot('main_destination')
+        slot_include_point = tracker.get_slot('include-point')
+        if slot_main_destination is None:
+            return [SlotSet("main_destination", entity_to)]
+        else:
+            if slot_include_point is not None:
+                slot_include_point.append(entity_include_point)
+            else:
+                slot_include_point = [entity_include_point]
+                return [SlotSet("include-point", entity_to)]
